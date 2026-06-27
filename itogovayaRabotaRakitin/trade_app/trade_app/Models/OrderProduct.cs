bd3198dbf6111843e42cf2b00a8333eb3bd176b2
m3tam0rff <vfrcbv18092004@gmail.com>
2026-06-27 21:08:26 +0300

    Add files via upload

---
+++ itogovayaRabotaRakitin/trade_app/trade_app/Models/OrderProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeApp.Models
+{
+    public class OrderProduct
+    {
+        public int OrderId { get; set; }
+        public string ProductId { get; set; } = null!;
+        public int Quantity { get; set; }
+        public virtual Order Order { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
---
+++ itogovayaRabotaRakitin/trade_app/trade_app/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeApp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int ManufacturerId { get; set; }
+        public int Discount { get; set; }
+        public int InStock { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; } = null!;
+    }
+}
